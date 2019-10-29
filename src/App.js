fe8240011d726ec413270031cb775c2d602f2f25
howlkp <howlkp@farmingdale.edu>
2019-10-29 18:34:40 -0400

    test

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Reserve from './pages/Reserve';
+import Donate from './pages/Donate';
+import Profile from './pages/Profile';
+import Wrapper from './components/wrapper/Wrapper';
+import './App.css';
+
+const App = () =>
+  (<Router>
+    <div>
+      <Wrapper>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/reserve" component={Reserve} />
+          <Route exact path="/donate" component={Donate} />
+          <Route exact path="/profile" component={Profile} />
+          <Route component={Home} /> 
+        </Switch>   
+      </Wrapper>    
+    </div>
+   </Router>);
+
+export default App;
---
+++ src/pages/Register.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NewUser from '../components/newuser/NewUser';
+
+const Register = () => (
+  <div className="container">
+    <NewUser />
+  </div>
+);
+
+export default Register;
